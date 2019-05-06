3d69b4ef90c572ef9fb2fd2b54a3d88b95368231
mitcon <koizumi741@gmail.com>
2019-05-06 19:02:51 +0900

    first commit

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    //プレイヤープレハブ
+    public GameObject playerPrefab;
+
+    //プレイヤー
+    private GameObject player;
+
+    //タイトル
+    private GameObject title;
+
+    //クリア
+    private GameObject clear;
+
+    //タイトル時カメラ
+    private GameObject titleCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+
+        title = GameObject.Find("Title");
+        clear = GameObject.Find("Clear");
+        clear.SetActive(false);
+        titleCamera = GameObject.Find("TitleCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsPlaying() == false && Input.GetKeyDown(KeyCode.Z)) {
+            GameStart();
+        }
+    }
+
+    void GameStart()
+    {
+        Debug.Log("GameStart");
+
+        if(clear.activeSelf){
+            clear.SetActive(false);
+        }
+        if(title.activeSelf){
+            title.SetActive(false);
+        }
+
+        player = Instantiate(playerPrefab,playerPrefab.transform.position,playerPrefab.transform.rotation);
+        
+        if(titleCamera.activeSelf){
+            titleCamera.SetActive(false);
+        }
+    }
+
+    public void GameOver(){
+        Debug.Log("GameOver");
+
+        title.SetActive(true);
+        titleCamera.SetActive(true);
+        Destroy(player);
+    }
+
+    public void GameClear(){
+        Debug.Log("GameClear");
+
+        clear.SetActive(true);
+        titleCamera.SetActive(true);
+
+        if(player.activeSelf){
+            Destroy(player);
+        }
+    }
+    public bool IsPlaying(){
+        return title.activeSelf == false && clear.activeSelf == false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+    public float rotateSpeed = 3f;
+    public float yLimit = -3f;
+
+    private CharacterController controller;
+    private Vector3 vector;
+    private GameObject manager;
+
+    void Start() {
+        //コンポーネントの取得
+        controller = GetComponent<CharacterController>();
+        manager = GameObject.Find("Manager");
+    }
+
+    void Update(){
+        //回転
+        transform.Rotate(0,Input.GetAxis("Mouse X"),0);
+
+        //キャラクターのローカル空間での方向
+        Vector3 forward = transform.transform.forward * Input.GetAxis("Vertical");
+        Vector3 right = transform.transform.right * Input.GetAxis("Horizontal");
+        
+        vector = forward + right;
+
+        //SimpleMove関数で移動させる
+        controller.SimpleMove(vector * speed);
+
+        if(transform.position.y < yLimit){
+            manager.GetComponent<Manager>().GameOver();
+        }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        if(hit.gameObject.tag == "Finish") {
+            manager.GetComponent<Manager>().GameClear();
+        }
+    }
+}
